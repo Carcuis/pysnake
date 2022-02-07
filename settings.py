671d9813d8984318910312af3ced9fe45c5b5b3d
Carcuis <ctx1256@foxmail.com>
2022-02-07 17:44:37 +0800

    Add val type hint

--- settings.py
+++ settings.py
@@ -4,45 +4,45 @@ from pygame.locals import *
 class Global:
     # UI
     # block size must be an integer
-    BLOCK_SIZE = 12
-    UI_SCALE = 2 * BLOCK_SIZE
+    BLOCK_SIZE: int = 12
+    UI_SCALE: int = 2 * BLOCK_SIZE
 
     # paddings must be an integer multiple of block size
-    TOP_PADDING = UI_SCALE
-    BOTTOM_PADDING = UI_SCALE
-    LEFT_PADDING = 0
-    RIGHT_PADDING = 0
+    TOP_PADDING: int = UI_SCALE
+    BOTTOM_PADDING: int = UI_SCALE
+    LEFT_PADDING: int = 0
+    RIGHT_PADDING: int = 0
 
     # grid count must be integers
-    GRID_COL = 80
-    GRID_ROW = 50
+    GRID_COL: int = 80
+    GRID_ROW: int = 50
 
     # do not change this variable
-    SCREEN_SIZE = (GRID_COL * BLOCK_SIZE + LEFT_PADDING + RIGHT_PADDING,
+    SCREEN_SIZE: tuple[int] = (GRID_COL * BLOCK_SIZE + LEFT_PADDING + RIGHT_PADDING,
                    GRID_ROW * BLOCK_SIZE + TOP_PADDING + BOTTOM_PADDING)
 
     # R, G, B
-    BACK_GROUND_COLOR = (31, 59, 100)
+    BACK_GROUND_COLOR: tuple[int] = (31, 59, 100)
 
-    FPS = 60
+    FPS: int = 60
 
     # snake
-    INIT_LENGTH = 3
+    INIT_LENGTH: int = 3
 
-    INIT_HEALTH = 8
-    MAX_HEALTH = 8
+    INIT_HEALTH: int = 8
+    MAX_HEALTH: int = 8
 
-    INIT_SATIETY = 8
-    MAX_SATIETY = 8
+    INIT_SATIETY: int = 8
+    MAX_SATIETY: int = 8
 
-    INIT_SPEED = 4
-    MIN_SPEED = 1
-    MAX_SPEED = 20
+    INIT_SPEED: int = 4
+    MIN_SPEED: int = 1
+    MAX_SPEED: int = 20
 
-    MAX_LEVEL = 10
+    MAX_LEVEL: int = 10
 
     # others
-    WALL_MAX_COUNT_IN_THOUSANDTHS = 20
+    WALL_MAX_COUNT_IN_THOUSANDTHS: int = 20
 
 
 class KeyBoard:
